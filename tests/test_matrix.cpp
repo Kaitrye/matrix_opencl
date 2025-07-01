4fc458f8389f38e283ad40da79e2eb690b2b739c
kaitrye <bogdanova.tekna156@yandex.ru>
2025-07-01 14:15:43 +0300

    Change tests wtih google test

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -1,25 +1,20 @@
-#include <cassert>
-#include <iostream>
+#include <gtest/gtest.h>
 #include <cstdlib>
 #include <ctime>
 #include "../src/matrix.h"
 
-void test_multiplication_small() {
+TEST(MatrixMultiplicationTest, SmallMatrix) {
     Matrix A(2, 3);
     Matrix B(3, 2);
 
     int value = 1;
-    for (int i=0; i<2; ++i) {
-        for (int j=0; j<3; ++j) {
+    for (int i = 0; i < 2; ++i)
+        for (int j = 0; j < 3; ++j)
             A(i, j) = value++;
-        }
-    }
 
-    for (int i=0; i<3; ++i) {
-        for (int j=0; j<2; ++j) {
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 2; ++j)
             B(i, j) = value++;
-        }
-    }
 
     Matrix expected(2, 2);
     expected(0, 0) = 58; expected(0, 1) = 64;
@@ -27,88 +22,64 @@ void test_multiplication_small() {
 
     Matrix C = A * B;
 
-    assert(C.rows() == 2 && C.cols() == 2);
+    ASSERT_EQ(C.rows(), 2);
+    ASSERT_EQ(C.cols(), 2);
+
     for (int i = 0; i < 2; ++i)
         for (int j = 0; j < 2; ++j)
-            assert(C(i, j) == expected(i, j));
-
-    std::cout << "test_multiplication_small passed\n";
+            EXPECT_EQ(C(i, j), expected(i, j));
 }
 
-void test_multiplication_by_identity() {
-
+TEST(MatrixMultiplicationTest, IdentityMatrix) {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    int N = 3;
+    const int N = 3;
     Matrix A(N, N);
     Matrix I(N, N);
 
-    for (int i=0; i<N; ++i) {
-        for (int j=0; j<N; ++j) {
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
             A(i, j) = rand();
-        }
-    }
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
         I(i, i) = 1;
-    }
 
     Matrix B = A * I;
 
     for (int i = 0; i < N; ++i)
         for (int j = 0; j < N; ++j)
-            assert(B(i, j) == A(i, j));
-
-    std::cout << "test_multiplication_by_identity passed\n";
+            EXPECT_EQ(B(i, j), A(i, j));
 }
 
-void test_multiply_dimension_mismatch() {
+TEST(MatrixMultiplicationTest, DimensionMismatchThrows) {
     Matrix A(2, 3);
     Matrix B(4, 2);
 
-    bool caught = false;
-    try {
+    EXPECT_THROW({
         A *= B;
-    } catch (const std::runtime_error& e) {
-        caught = true;
-    }
-
-    assert(caught);
-    std::cout << "test_multiply_dimension_mismatch passed\n";
+    }, std::runtime_error);
 }
 
-void test_1x1_matrix() {
+TEST(MatrixMultiplicationTest, OneByOneMatrix) {
     Matrix A(1, 1);
     Matrix B(1, 1);
     A(0, 0) = 7;
     B(0, 0) = 3;
 
     Matrix C = A * B;
-    assert(C.rows() == 1 && C.cols() == 1);
-    assert(C(0, 0) == 21);
 
-    std::cout << "test_1x1_matrix passed\n";
+    ASSERT_EQ(C.rows(), 1);
+    ASSERT_EQ(C.cols(), 1);
+    EXPECT_EQ(C(0, 0), 21);
 }
 
-void test_zero_matrix() {
+TEST(MatrixMultiplicationTest, ZeroMatrixResult) {
     Matrix A(2, 3);
     Matrix B(3, 4);
+
     Matrix C = A * B;
 
     for (size_t i = 0; i < C.rows(); ++i)
         for (size_t j = 0; j < C.cols(); ++j)
-            assert(C(i, j) == 0);
-
-    std::cout << "test_zero_matrix passed\n";
-}
-
-int main() {
-    test_multiplication_small();
-    test_multiplication_by_identity();
-    test_multiply_dimension_mismatch();
-    test_1x1_matrix();
-    test_zero_matrix();
-
-    std::cout << "All tests passed!\n";
-    return 0;
+            EXPECT_EQ(C(i, j), 0);
 }
